6cbd52d437a1e1c26023e494b1a0854df520ccc7
eamiear <justcall_me_k@163.com>
2020-06-17 15:26:27 +0800

    import hcc

---
+++ src/views/mine/Children/MyOrderChildren/AfterSale.vue
@@ -0,0 +1,30 @@
+<!--
+ * @Description: 个人中心->我的订单->售后
+ -->
+<template>
+  <div id="afterSale">
+
+  </div>
+</template>
+
+<script type="text/javascript">
+export default {
+  mounted () {
+
+  },
+  data () {
+    return {
+
+    }
+  },
+  components: {
+
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="less" scoped>
+</style>
